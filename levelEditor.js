676bb43ddce94671515b81071f5231a3693bb1d3
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-07-25 12:38:37 -0400

    mouse pos and canvas scroll

--- levelEditor.js
+++ levelEditor.js
@@ -1,1 +1,14 @@
 'use strict';
+function startEditor(startData, { canvas }) {
+	canvas.addEventListener('mousemove', function (e) {
+		let rect = this.getBoundingClientRect();
+		let spriteX = Math.floor((e.pageX - this.offsetLeft) * (this.offsetWidth / rect.width / getComputedStyle(this).getPropertyValue('zoom')) - rect.left);
+		let spriteY = Math.floor((e.pageY - this.offsetTop) * (this.offsetHeight / rect.height / getComputedStyle(this).getPropertyValue('zoom')) - rect.top);
+		let tileX = Math.floor(spriteX / TILE_WIDTH);
+		let tileY = Math.floor(spriteY / TILE_HEIGHT);
+		console.log(tileX, tileY);
+	});
+	start(startData);
+	canvas.width = level.width * TILE_WIDTH;
+	canvas.height = level.height * TILE_HEIGHT;
+}
